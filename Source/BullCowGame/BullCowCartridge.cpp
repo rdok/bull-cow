f8687dd197adef2290594b434c4a23011166edb8
Rizart Dokollari <r.dokollari@gmail.com>
2019-12-14 14:35:23 +0000

    Implement bsaic terminal game loop. Resolve #3.

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -1,17 +1,25 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
+#include <string>
 
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
 
-    PrintLine("Pioneer, the first satellite launched.");
-    PrintLine("What was the launch year of Pioneer probe?");
+    PrintLine(TEXT("Pioneer, the first satellite launched."));
+    PrintLine(TEXT("What was the launch year of Pioneer probe?"));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-    FString Isogram = "spacecraft";
+    FString Isogram = TEXT("spacecraft");
 
     ClearScreen();
+    PrintLine(Input);
+
+    FString message = Input == Isogram 
+        ? TEXT("correct")
+        : TEXT("incorrect");
+
+    PrintLine(message);
 }
