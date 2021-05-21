12dca74169a0bf3f68373c279c9e13ebc0b326bb
Rizart Dokollari <r.dokollari@gmail.com>
2021-05-21 05:40:12 +0100

    WIP: updating deploy command.

--- deploy.bash
+++ deploy.bash
@@ -1,11 +1,12 @@
-
 #!/bin/bash
 
-aws s3 sync ./html-build/HTML5 s3://pioneer-0 --delete \
-    --exclude "*" \
-    --include "*.js" \
-    --include "*.wasm" \
-    --include "*.data" \
-    --include "*.html" \
-    --include "Utility.js" \
-    --include ".htaccess"
+sam build
+
+#aws s3 sync ./html-build/HTML5 s3://pioneer-0 --delete \
+#    --exclude "*" \
+#    --include "*.js" \
+#    --include "*.wasm" \
+#    --include "*.data" \
+#    --include "*.html" \
+#    --include "Utility.js" \
+#    --include ".htaccess"
