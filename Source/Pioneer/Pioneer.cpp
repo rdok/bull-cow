fcaede4b50bf751a22265100d121810d0dc7acd8
Rizart Dokollari <r.dokollari@gmail.com>
2021-10-17 09:40:55 +0100

    Rename BullCowGame identifiers to Pioneer

--- Source/Pioneer/Pioneer.cpp
+++ Source/Pioneer/Pioneer.cpp
@@ -1,6 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "BullCowGame.h"
+#include "Pioneer.h"
 #include "Modules/ModuleManager.h"
 
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BullCowGame, "BullCowGame" );
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Pioneer, "Pioneer" );
