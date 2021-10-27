60827efdb34acb010cb8170112776a9cdae7b2b0
Rizart Dokollari <r.dokollari@gmail.com>
2021-10-27 05:47:02 +0100

    Boyscout

--- Source/Pioneer/PioneerGameModeBase.h
+++ Source/Pioneer/PioneerGameModeBase.h
@@ -1,17 +1,11 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
 #include "PioneerGameModeBase.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class PIONEER_API APioneerGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-	
 };
